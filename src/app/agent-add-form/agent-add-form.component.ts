2cfc4ccc13676886af117cb2aee75c9cad1a0f29
BraHim WaHman <brahim@BraHims-MacBook-Pro.local>
2023-06-12 14:53:56 +0100

    first commit

---
+++ src/app/agent-add-form/agent-add-form.component.ts
@@ -0,0 +1,123 @@
+import { AddFileRequest } from './../Interfaces(Modules)/AddFileRequest';
+import { HttpClient } from '@angular/common/http';
+import { Agent } from '../Interfaces(Modules)/Agent';
+import { Component, Inject, OnInit } from '@angular/core';
+import { AdminService } from '../_services/admin.service';
+import { Router } from '@angular/router';
+import { TokenStorageService } from '../_services/token-storage.service';
+
+
+@Component({
+  selector: 'app-agent-add-form',
+  templateUrl: './agent-add-form.component.html',
+  styleUrls: ['./agent-add-form.component.css']
+})
+
+export class AgentAddFormComponent implements OnInit {
+
+  agent ={
+    agentId:0,
+    agentPassword:"",
+    firstName:"",
+    lastName:"",
+    emailAddress:"",
+    dateOfBirth:"",
+    homeAddress:"",
+    phoneNumber:"",
+    identityCardNumber:"",
+    identityJustification:"",
+    isFirstLogIn:true,
+    patentNumber:"",
+    commerceregistrySerialNumber:"",
+  };
+
+  addfileRequest:AddFileRequest={
+    file_id:0,
+    name:"",
+    owneruid:"",
+    description:"",
+    type:"",
+    file:null,
+  }
+
+  file:File;
+  owneruid:string;
+  description:string;
+
+
+  IdentTypes:string[] = ["C.I.N","Passport","Driver License"];
+  errorMessage:String;
+  currentAdmin:any;
+
+
+  constructor(
+    private http:HttpClient,
+    private adminService:AdminService,
+    private token: TokenStorageService,
+    private router: Router,
+    private tokenStorage:TokenStorageService
+    ) {
+     
+   }
+
+  ngOnInit(): void {
+    if(this.tokenStorage.getToken()==null){
+      this.router.navigate(['admin/logIn']);
+    }
+     this.currentAdmin=this.token.getAdmin();
+    
+  }
+
+  submitAgentAddForm(form:any){
+   
+     console.log(form);
+  if(form.valid){
+
+  this.adminService.saveAgent(
+    this.agent.firstName,
+    this.agent.lastName,
+    this.agent.emailAddress,
+    this.agent.identityJustification,
+    this.agent.identityCardNumber,
+    this.agent.dateOfBirth,
+    this.agent.phoneNumber,
+    this.agent.commerceregistrySerialNumber,
+    this.agent.patentNumber,
+    this.agent.homeAddress,
+    ).subscribe({
+      next: data => {
+        console.log("agent saved successfully");
+        this.owneruid=data.username;
+        console.log(data.username);
+
+      
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+       
+      }
+     
+    });
+
+    /*
+    this.adminService.saveAgentFile(this.file,this.owneruid,this.description).subscribe({
+      next: data => {
+        console.log("agent file saved successfully");
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        console.log("erreur while uploading file")
+       
+      }
+     
+    });
+    
+    /*
+
+
+   window.location.reload();*/
+   
+  }
+}
+  
+}
